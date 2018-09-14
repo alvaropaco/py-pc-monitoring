f7555275e83f6a3fcd4894a4c5f50c6102332072
Alvaro Paçó <alvaro@dittomusic.com>
2018-09-14 18:34:52 -0300

    Persistence, MAC address and startedAt attributes

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from datetime import datetime
 from pynput import mouse
 from store import Store
+from uuid import getnode as get_mac
 
 # Connection URL
 url = 'https://8b6a2180-4759-4464-9d7d-cb02cd512c19-bluemix:112fc643a14a9ea5c38732cd5abdf95d3ff40bd8bf15db73107d8089069b38fa@8b6a2180-4759-4464-9d7d-cb02cd512c19-bluemix.cloudant.com'
 
 file = open("testfile.txt","w") 
 
-startAt = now = datetime.now()
+startAt = datetime.now()
+me = get_mac()
 currentDateTime = datetime.now()
 
 def on_move(x, y):
     currentDateTime = datetime.now()
-    strAt = '{0} to {1}\n'.format(startAt, currentDateTime)
-    # print(strAt)
-    file.write(strAt)
+    record = {
+        'startedAt': str(startAt),
+        'datetime': str(currentDateTime),
+        'MACaddr' : me
+    }
+    doc_id, doc_rev = store.save(record)
+    print(record)
 
 def on_click(x, y, button, pressed):
-    strAt = 'Pointer moved to {0} at {1}\n'.format(
-        (x, y), currentDateTime)
-    file.write(strAt)
+    currentDateTime = datetime.now()
+    doc_id, doc_rev = store.save({
+        'startedAt': str(startAt),
+        'datetime': str(currentDateTime),
+        'MACaddr' : me
+    })
     if not pressed:
         # Stop listener
         return True
 
 def on_scroll(x, y, dx, dy):
     currentDateTime = datetime.now()
-    strAt = 'Scrolled {0} to {1} at {2}'.format(
-        'down' if dy < 0 else 'up',
-        (x, y), currentDateTime)
-    
-    store.save({'datetime': currentDateTime})
-    # file.write(strAt)
-    # print('Scrolled {0} to {1} at {2}'.format(
-    #     'down' if dy < 0 else 'up',
-    #     (x, y), currentDateTime))
+    doc_id, doc_rev = store.save({
+        'startedAt': str(startAt),
+        'datetime': str(currentDateTime),
+        'MACaddr' : me
+    })
 
 def main():
     # Collect events until released
--- store.py
+++ store.py
@@ -4,12 +4,10 @@ class Store:
 
     def __init__(self, url, db):
         self.conn = couchdb.client.Server(url)
-        #self.conn = couchdb.Server(host + ':' + password + '@' + host + ':' + port)
         self.db = self.conn[db]
 
     def __str__(self):
         return self.conn.version()
 
-    def save(doc):
-        print(doc)
-        self.db.save(doc)
+    def save(self, doc):
+        return self.db.save(doc)
